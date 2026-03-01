51851d391000e560637146cb2b1f9d2575dd560a
jithsungh <jithsunghsai@gmail.com>
2026-03-02 01:47:34 +0530

    fix: separate export of Scene3D and RenderState3D for clarity

--- packages/desktop/src/renderer3d/index.ts
+++ packages/desktop/src/renderer3d/index.ts
@@ -1,4 +1,5 @@
-export { Scene3D, RenderState3D } from './Scene3D';
+export { Scene3D } from './Scene3D';
+export type { RenderState3D } from './Scene3D';
 export { Table } from './Table';
 export { Ball } from './Ball';
 export { Net } from './Net';
